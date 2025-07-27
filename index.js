4c7cf2490f1b9138fb5a3c210774360b98111856
dhiravpatel <dhiravpatel07@gmail.com>
2025-07-27 17:00:34 +0530

    add the cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const {sendBulkEmails} = require('./controller/BulkMailController')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4242
 
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.post('/api/send_bulk_mail', sendBulkEmails)
 
