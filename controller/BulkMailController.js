8d5a3f718520ceff99303c1ca6f9b4603be52fdf
dhiravpatel <dhiravpatel07@gmail.com>
2025-07-27 16:25:28 +0530

    modification in templates

--- controller/BulkMailController.js
+++ controller/BulkMailController.js
@@ -27,7 +27,36 @@ async function sendEmail(to, subject, html) {
 
 async function sendBulkEmails(req, res) {
   try {
-    const { emails, subject, html } = req.body;
+    const { emails, subject, message } = req.body;
+
+    const STATIC_HTML =`
+    ${message}
+    <div style="display: flex;">
+      <img src="https://nyc.cloud.appwrite.io/v1/storage/buckets/68681a5a0000f82fcbdb/files/6886000b001127585c50/view?project=686815b8002cdbf9d18d&mode=admin" width="130" height="70" style="margin-right:10px;" />
+      <div>
+        <p style="margin:0px; margin-top:10px;">
+          Address:
+          <a href="https://maps.app.goo.gl/6hhBpRE75BUJRLDZA">
+            F-4, First Floor, Latest Ceramic Zone, B/H Ishan Complex, 8-A National Highway, Lalpar, Morbi-363642, Gujarat, India
+          </a>
+        </p>
+        <p style="margin:0px;">
+          Contact:
+          <a href="tel:+919327624243">
+            +91 9327624243
+          </a>
+        </p>
+        <p style="margin:0px;">
+          Web:
+          <a href="https://asios.in">
+            www.asios.in
+          </a>
+        </p>
+      </div>
+    </div>
+    </div>
+        </div>\n</div>`;
+
 
     if (!emails || !Array.isArray(emails) || emails.length === 0) {
       return res.status(400).json({
@@ -36,17 +65,18 @@ async function sendBulkEmails(req, res) {
       });
     }
 
-    if (!subject || !html) {
+    if (!subject || !message) {
       return res.status(400).json({
         success: false,
-        message: 'Subject and HTML content are required'
-      });
+        message: 'Subject or message is required'
+      });   
     }
 
+    // 2. Use STATIC_HTML instead of html from req.body
     emails.forEach((email, index) => {
       setTimeout(() => {
-        sendEmail(email, subject, html);
-      }, index * 10000); // 10 seconds interval
+        sendEmail(email, subject, STATIC_HTML);
+      }, index * 2000);
     });
 
     console.log(`🚀 Bulk email job scheduled. All emails spaced 10 seconds apart.`);
